c8ab67fae7e1e94da2c8714ddf28053767acc41d
CDAMIAN <cdamian@EDCMXENLT0599.local>
2021-08-10 16:49:59 -0500

    add theme reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {configureStore} from '@reduxjs/toolkit'
-import {Provider}from 'react-redux'
-import userReducer from './features/user'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import userReducer from "./features/user";
+import themeReducer from "./features/theme";
+
 const store = configureStore({
   reducer: {
     user: userReducer,
+    theme: themeReducer,
   },
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
